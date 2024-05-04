7da1fa8bbef4787528289a421e3af81483e4d661
Crypto7816 <nachuan.shi.quant@gmail.com>
2024-05-04 20:07:42 +0800

    chore: Remove unnecessary code for processing CSV files

--- process.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_csv('csv_file/telegram_history_bug.csv')
-df2 = pd.read_csv('csv_file/telegram_history_match.csv')
-
-df = pd.concat([df1, df2], ignore_index=True)
-df.to_csv('csv_file/data.csv', index=False)
